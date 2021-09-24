29beeab28c45734463e0ccec5ef4839f3c658820
ophir.h <ophir.h@quali.com>
2021-09-24 09:49:22 +0300

    fix conflict

--- rds-mysql/variables.tf
+++ rds-mysql/variables.tf
@@ -1,19 +1,15 @@
 variable "db_name" {
   description = "db name"
-  default = "demo_db"
 }
 
 variable "username" {
   description = "User name"
-  default = "wordpress"
 }
 
 variable "password" {
   description = "Database password"
-  default = "Torque!123"
 }
 
 variable "sandbox_id" {
   description = "Colony sandbox id"
-  default = "123"
 }
