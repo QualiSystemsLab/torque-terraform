a5bbe1acfd5fec5d2a1309b2a7584643850bfbb6
ophir.h <ophir.h@quali.com>
2021-09-29 12:04:44 +0300

    make rds instance public

--- rds-mysql/network.tf
+++ rds-mysql/network.tf
@@ -22,7 +22,24 @@ resource "aws_subnet" "b" {
     map_public_ip_on_launch = true
 }
 
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table" "all" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.a.id
+  route_table_id = aws_route_table.all.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.b.id
+  route_table_id = aws_route_table.all.id
 }
